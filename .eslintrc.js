68a6e3355e3e99fd28e5f84ec0964a165b83c119
wrynnsun <wrynnsun@tencent.com>
2019-08-21 22:17:00 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,25 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    "plugin:vue/recommended"
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "semi":[1,'never'],
+    "no-console": "off",
+    "indent":[2, 4,{SwitchCase: 1}],
+
+    //vue插件的配置
+    "vue/html-indent":[1,4],
+    "vue/no-v-html":0
   },
   parserOptions: {
     parser: 'babel-eslint'
